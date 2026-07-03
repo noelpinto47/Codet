56e7b91bd687dbd4e27d0c9d062f7553b37afd79
Noel Pinto <noelpinto47@gmail.com>
2026-07-04 00:15:48 +0530

    added builder for macos

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -3,11 +3,18 @@ pub mod state;
 pub mod update;
 pub mod view;
 
-use iced::Theme;
+use iced::{window, Theme};
 
 pub fn run() -> iced::Result {
+    let icon = window::icon::from_file_data(include_bytes!("../../assets/icon.png"), None)
+        .expect("failed to load application icon");
+
     iced::application(state::AppState::default, update::update, view::view)
         .title("Codet")
+        .window(window::Settings {
+            icon: Some(icon),
+            ..Default::default()
+        })
         .theme(Theme::Dark)
         .run()
 }
